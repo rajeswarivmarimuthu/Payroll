4db0d54bd0914d4ef745732abf85eb5d41a2a910
rajeswari marimuthu <rajeswarivmarimuthu@gmail.com>
2022-04-07 20:01:42 -0700

    Pushing local storage changes + populate reacent searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,7 @@ var charThumbnail = document.getElementById("charImage");
 var charName = document.getElementById("charName");
 var charDescription = document.getElementById("charDescription");
 var charComics = document.getElementById("charComics");
+var recentSearchContainerEl = document.getElementById('recentSearchContainer');
 
 var movieContainerEl = document.getElementById("moviesContainer");
 
@@ -21,15 +22,14 @@ var buttonEl = document.getElementById("submitBtn");
 
 // event listeners
 buttonEl.addEventListener("click", searchId);
-// TODO history event listeners
+recentSearchContainerEl.addEventListener("click",searchId);
 
 // initialize page
 init();
 
 // functions
 function init() {
-    getLast5FromLocalStorage();
-    appendLast5();
+    populateRecentSearches();
 }
 
 // main search function
@@ -61,6 +61,11 @@ function searchCharacter(id) {
         return response.json();
     })
     .then(function (d) {
+
+        if (typeof(d.data.results[0].urls[2].url) == 'undefined' ) {
+            d.data.results[0].urls[2].url = ' '
+        };
+
         var characterObj = {
             name: d.data.results[0].name,
             description: d.data.results[0].description,
@@ -69,6 +74,7 @@ function searchCharacter(id) {
         };
 
         addToLocalStorage(characterObj)
+
 
         charThumbnail.setAttribute("src", characterObj.thumbnail);
         charThumbnail.setAttribute("alt", characterObj.name);
@@ -124,35 +130,51 @@ function searchMovie(query) {
 
 // sets character object to local storage
 function addToLocalStorage(characterObj) {
-    var searchedCharacters = JSON.parse(localStorage.getItem("searched_characters"));
-    if (searchedCharacters == null) {
+    var searchedCharacters = [];
+    searchedCharacters = JSON.parse(localStorage.getItem("searched_characters"));
+    var charInLs;
+    
+    if (!searchedCharacters) {
         searchedCharacters = []
-    };
-    // TODO duplicate searches
-    localStorage.setItem("character", JSON.stringify(characterObj));
+    }
+    
+    // Checking for duplicates and if present skip storing in localStorage!! 
+    charInLs = searchedCharacters.findIndex(e => e.name == characterObj.name)
+    console.log(searchedCharacters, characterObj.name, charInLs);
+    if (charInLs >=0) {
+        return;
+    }
     searchedCharacters.push(characterObj);
+    localStorage.setItem("character", JSON.stringify(characterObj));
+
+    // Storing only 5 Marvel characters in localStorage
+    if (searchedCharacters.length === 6) {
+         searchedCharacters.shift();
+    }
+    
     localStorage.setItem("searched_characters", JSON.stringify(searchedCharacters));
+    
 };
 
-// gets last 5 character objects from local storage
-function getLast5FromLocalStorage() {
+// Populate recent searches from local storage
+function populateRecentSearches() {
     var storedCharacters = JSON.parse(localStorage.getItem("searched_characters"));
+    recentSearchContainerEl.innerHTML ='';
     if (storedCharacters == null) {
         storedCharacters = [];
-    } else if (storedCharacters.length > 5) {
-        storedCharacters = storedCharacters.slice(-5);
-    };
+        return;
+    } 
     if (storedCharacters.length > 0) {
-        var j = 0;
-        var characterOrderDesc = [];
-        for (let i = storedCharacters.length - 1; j < storedCharacters.length & j < 5; i--) {
-            characterOrderDesc.push(storedCharacters[i]);
-            j++;
+        for (let i = storedCharacters.length - 1; i < storedCharacters.length; i--) {
+            var imgDiv = document.createElement('div');
+            imgDiv.setAttribute ('class', "max-w-sm rounded-full h-20 w-20 border border-red-200 overflow-hidden hover:border-red-200 hover:bg-red-100 transition duration-200 hover:scale-105 shadow-2xl");
+            var charImg = document.createElement('img');
+            charImg.setAttribute('class',"w-full");
+            charImg.setAttribute ('src', storedCharacters[i].thumbnail);
+            charImg.setAttribute('alt', storedCharacters[i].name);
+
+            imgDiv.appendChild(charImg);
+            recentSearchContainerEl.appendChild(imgDiv);
         }
     }
-    return characterOrderDesc;
-}
-
-function appendLast5() {
-    // TODO
 }
